88f745335dc21f14725aaead72ff958e57062c95
Nancy Decouttere <Nancy.Decouttere@realdolmen.com>
2020-12-10 13:37:40 +0100

    wip

--- frontend/RMotownFestival/src/environments/environment.prod.ts
+++ frontend/RMotownFestival/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  festivalName: "RMotownFestival",
+  festivalName: "RMotownFestivalName",
   apiBaseUrl: "https://rmotownfestivalapiNDC.azurewebsites.net/api/"
 };
